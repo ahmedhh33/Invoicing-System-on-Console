72b1dba0a47b88c2d8779171119096389dcb6537
ahmedhh33 <ahmedhh3633@gmail.com>
2023-08-07 21:33:57 +0400

    Adding all code again becouse in last time was not saved

--- ShopApplication.cs
+++ ShopApplication.cs
@@ -3,10 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Invoicing_System_on_Console
 {
-    internal class ShopApplication
+    //Implementing the main application class
+    public class ShopApplication
     {
+        private const string DATA_FILE = "shopdata.json";
+        private Shop shop;
+
+        public ShopApplication() 
+        {
+            //the data needs to be loaded from a storage medium (JSON file) before the application starts.
+            LoadData();
+        }
+        private void ShopSettingsMenu()
+        {
+            while (true)
+            {
+                Menu.Show(new string[]
+                {
+                "Load Data (Items and invoices)",
+                "Set Shop Name",
+                "Set Invoice Header (Tel / Fax / Email / Website)",
+                "Go Back"
+                });
+
+                int choice = GetMenuChoice(4);
+                switch (choice)
+                {
+                    case 1:
+                        LoadData();
+                        break;
+                    case 2:
+                        SetShopName();
+                        break;
+                    case 3:
+                        SetInvoiceHeader();
+                        break;
+                    case 4:
+                        return;
+                }
+            }
+        }
+
+        private void LoadData()
+        {
+            if (File.Exists(DATA_FILE))
+            {
+                string jsonData = File.ReadAllText(DATA_FILE);
+                shop = JsonConvert.DeserializeObject<Shop>(jsonData);
+                //it will return the deserialaysed object from JSON string
+
+            }
+            else
+            {
+                //If the file does not exist, it initializes a new shop object with default values.
+                //so the shop object is prepared for use in the application in eather ways
+                shop = new Shop
+                {
+                    ShopName="Tilal Abueid Grocery Shop",
+                    InvoiceHeader = "Tele: 22343238 / Email: Tilal_Abueid@gmail.com | Website: www.TilalAbueidGrocery.com",
+                    Items = new List<SingleItem>(),
+                    Invoices = new List<Invoice>()
+                };
+
+            }
+            Console.WriteLine("Data loaded successfully.");
+        }
+        private void SaveData()
+        {
+            try
+            {
+                //here it convert the object to string to save it in json datafile serialaizing
+                string jsonData = JsonConvert.SerializeObject(shop, Formatting.Indented);
+                File.WriteAllText(DATA_FILE, jsonData);
+                Console.WriteLine("Data saved successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while saving data: " + ex.Message);
+            }
+        }
+        private void SetShopName()
+        {
+            Console.WriteLine("Please Enter the New Name of the Shop : ");
+            shop.ShopName = Console.ReadLine();
+            SaveData();//it save the new data changed
+        }
+        private void SetInvoiceHeader()
+        {
+            Console.WriteLine("Set the Invoice header (Tele/Fax/Email/Website): ");
+            shop.InvoiceHeader = Console.ReadLine();
+            SaveData();
+        }
     }
 }
