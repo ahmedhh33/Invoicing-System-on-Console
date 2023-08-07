fddb7eccfbe47ca3235e0510bc060ea403058158
ahmedhh33 <ahmedhh3633@gmail.com>
2023-08-07 10:52:14 +0400

    Invoice class adding

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Invoicing_System_on_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
